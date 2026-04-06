8638dfc6b37349ef32351fa70022082fac24d79a
ProCAD User <procad@example.com>
2026-04-06 23:03:59 +0530

    Add Android project structure and WebView app for ProCAD.html

---
+++ app/src/main/java/com/example/procad/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.procad;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/ProCAD.html");
+    }
+}
